127936ad2a57e26324c647302084c07bcaed213b
Toru <toru.inoue@kissaki.tv>
2015-03-03 14:43:43 +0900

    portable.

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-# tell nginx's build system where to find LuaJIT 2.1:
-# ふーむlibってどこよ、、、2.1でlibつくる方法が知りたい。
+projectpath=$(pwd)
 
-# 今回はまずやりたいことができるかどうかのチェックのために、luajit2.0.xをbrewで入れて動かす。
-export LUAJIT_LIB=/usr/local/Cellar/luajit/2.0.3_1/lib
-export LUAJIT_INC=/usr/local/Cellar/luajit/2.0.3_1/include/luajit-2.0
+# test with 2.0.3_1. we'll update it with 2.1.x 
+export LUAJIT_LIB=$projectpath/luajit-2.0.3_1/2.0.3_1/lib
+export LUAJIT_INC=$projectpath/luajit-2.0.3_1/2.0.3_1/include/luajit-2.0
 
+# same folder contains below.
 NGX_DEVEL_KIT="ngx_devel_kit-0.2.19"
 LUA_NGX_MOD="lua-nginx-module-0.9.15"
 
 # Here we assume Nginx is to be installed under /opt/nginx/.
-./configure --prefix=/Users/illusionismine/Desktop/nginx-luajit/bin \
+./configure --prefix=$projectpath/bin \
 	--with-ld-opt='-Wl,-rpath,$LUAJIT_LIB' \
 	--add-module=$NGX_DEVEL_KIT \
 	--add-module=$LUA_NGX_MOD	
