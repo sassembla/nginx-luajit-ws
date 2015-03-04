718e87ae656b44de5c8e4eef838bf26136275ba7
Toru <toru.inoue@kissaki.tv>
2015-03-04 11:12:49 +0900

    add pcre, delete temps(objs)

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ export LUAJIT_INC=$projectpath/luajit-2.0.3_1/2.0.3_1/include/luajit-2.0
 NGX_DEVEL_KIT="ngx_devel_kit-0.2.19"
 LUA_NGX_MOD="lua-nginx-module-0.9.15"
 
-# Here we assume Nginx is to be installed under /opt/nginx/.
+# make & install nginx to PROJECT_PATH/bin
 ./configure --prefix=$projectpath/bin \
 	--with-ld-opt='-Wl,-rpath,$LUAJIT_LIB' \
 	--add-module=$NGX_DEVEL_KIT \
-	--add-module=$LUA_NGX_MOD	
+	--add-module=$LUA_NGX_MOD \
+	--with-pcre=pcre-8.36
 
 make -j2
 make install
