1cb6b40531b2f25eaeb4785c7b9a7d89e9c5b508
Toru <toru.inoue@kissaki.tv>
2015-03-20 12:18:16 +0900

    update: no need to luajit dependentcy from outside.

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-projectpath=$(pwd)
+PROJECT_PATH=$(pwd)
 
-# luajit requires from Lua-nginx module
-export LUAJIT_LIB=$projectpath/luajit-2.1/lib
-export LUAJIT_INC=$projectpath/luajit-2.1/include/luajit-2.1
+# build luajit 2.1 first
+LUAJIT_FOLDER="luajit-2.1"
+cd $LUAJIT_FOLDER
+sh build.sh
+cd ../
+
+# luajit required from Lua-nginx module. set export.
+export LUAJIT_LIB=$PROJECT_PATH/$LUAJIT_FOLDER/lib
+export LUAJIT_INC=$PROJECT_PATH/$LUAJIT_FOLDER/include/luajit-2.1
 
 # same folder contains below.
 NGX_DEVEL_KIT="ngx_devel_kit-0.2.19"
@@ -11,7 +17,7 @@ HTTP_REDIS_MOD="ngx_http_redis-0.3.7"
 PCRE="pcre-8.36"
 
 # make & install nginx to PROJECT_PATH/bin
-./configure --prefix=$projectpath/bin \
+./configure --prefix=$PROJECT_PATH/bin \
 	--with-ld-opt='-Wl,-rpath,$LUAJIT_LIB' \
 	--add-module=$NGX_DEVEL_KIT \
 	--add-module=$LUA_NGX_MOD \
@@ -20,3 +26,6 @@ PCRE="pcre-8.36"
 
 make -j2
 make install
+
+# use local luajit dylib.
+install_name_tool -change /usr/local/lib/libluajit-5.1.2.dylib $PROJECT_PATH/bin/lib/libluajit-5.1.2.1.0.dylib bin/sbin/nginx
