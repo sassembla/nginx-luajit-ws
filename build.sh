177f9a90b3bc2a94eb69dbf18d4723c2f81557ec
Toru <toru.inoue@kissaki.tv>
2015-03-28 16:27:45 +0900

    add zlib

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
+# basically these settings are for Mac OS X.
+
 PROJECT_PATH=$(pwd)
+
+NGINX_VERSION=1.7.10
+
+rm $NGINX_VERSION/sbin/nginx
 
 # build luajit 2.1 first
 LUAJIT_FOLDER="luajit-2.1"
 cd $LUAJIT_FOLDER
 sh build.sh
 cd ../
+
+#install dylib to nginx/lib
+cp $LUAJIT_FOLDER/lib/libluajit-5.1.2.1.0.dylib $NGINX_VERSION/lib
+
 
 # luajit required from Lua-nginx module. set export.
 export LUAJIT_LIB=$PROJECT_PATH/$LUAJIT_FOLDER/lib
@@ -14,18 +24,27 @@ export LUAJIT_INC=$PROJECT_PATH/$LUAJIT_FOLDER/include/luajit-2.1
 NGX_DEVEL_KIT="ngx_devel_kit-0.2.19"
 LUA_NGX_MOD="lua-nginx-module-0.9.15"
 HTTP_REDIS_MOD="ngx_http_redis-0.3.7"
+ZLIB="zlib"
 PCRE="pcre-8.36"
 
-# make & install nginx to PROJECT_PATH/bin
-./configure --prefix=$PROJECT_PATH/bin \
-	--with-ld-opt='-Wl,-rpath,$LUAJIT_LIB' \
+# 実行時にlibluajitを特定の位置から呼んでほしいんだが、コンパイル時に下記を指定しても実行時の読み込み箇所が変わらない(usr/local/libを探す)ので、困っている。
+# --with-ld-opt="-rpath,$LUAJIT_LIB" \
+# →調べてみたら、nginx-lua-moduleがconfigでその辺やってるっぽい。
+
+# make & install nginx to PROJECT_PATH/NGINX_VERSION
+./configure \
+	--prefix=$PROJECT_PATH/$NGINX_VERSION \
 	--add-module=$NGX_DEVEL_KIT \
 	--add-module=$LUA_NGX_MOD \
 	--add-module=$HTTP_REDIS_MOD \
+	--with-zlib=$ZLIB \
 	--with-pcre=$PCRE
 
 make -j2
 make install
 
-# use local luajit dylib.
-install_name_tool -change /usr/local/lib/libluajit-5.1.2.dylib $PROJECT_PATH/bin/lib/libluajit-5.1.2.1.0.dylib bin/sbin/nginx
+otool -L $NGINX_VERSION/sbin/nginx
+
+# for Mac. use local luajit dylib. rewrite link.
+install_name_tool -change /usr/local/lib/libluajit-5.1.2.dylib $PROJECT_PATH/$NGINX_VERSION/lib/libluajit-5.1.2.1.0.dylib $NGINX_VERSION/sbin/nginx
+
